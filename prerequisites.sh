4afdcfbf173a7fa66a0a0511ee1a7c4d78a2829d
Your Name <you@example.com>
2020-03-17 22:13:17 +0100

    travis

--- prerequisites.sh
+++ prerequisites.sh
@@ -8,7 +8,7 @@ sudo apt-get install libdbus-1-dev
 sudo apt-get install libudisks2-dev
 sudo apt-get install libglib2.0-dev
 sudo apt-get install libpam0g-dev
-sudo apt-get install python-pip
+sudo apt-get install python-pip || true
 
 
 
